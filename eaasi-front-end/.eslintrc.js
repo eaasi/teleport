817e6dbb137b86a36f571c300ff4125d3fa9c9ec
Brandon Wiemann <brandon@portalmedia.com>
2019-06-26 10:41:24 -0500

    wip - style and syntax rules

--- eaasi-front-end/.eslintrc.js
+++ eaasi-front-end/.eslintrc.js
@@ -1,18 +1,64 @@
+
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-    '@vue/typescript',
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-  },
-  parserOptions: {
-    parser: '@typescript-eslint/parser',
-  },
+	root: true,
+	env: {
+		browser: true,
+		es6: true
+	},
+	extends: ['plugin:vue/strongly-recommended', '@vue/typescript'],
+	globals: {
+		Atomics: 'readonly',
+		SharedArrayBuffer: 'readonly'
+	},
+	parserOptions: {
+		ecmaVersion: 2018,
+		sourceType: 'module',
+		parser: '@typescript-eslint/parser',
+	},
+	plugins: [
+		'vue'
+	],
+	rules: {
+		indent: ['error', 'tab'],
+		'lines-between-class-members': ['warn', 'always'],
+		'vue/component-name-in-template-casing': ['error', 'kebab-case'],
+		'vue/order-in-components': [
+			'error', {
+				'order': [
+					'el',
+                    'name',
+                    'extends',
+					'functional',
+					'mixins',
+					'inheritAttrs',
+                    'model',
+					'props',
+					['components', 'directives', 'filters'],
+					'computed',
+					'data',
+					'methods',
+					'LIFECYCLE_HOOKS',
+					'watch'
+				]
+			}
+		],
+		'vue/html-indent': ['warn', 'tab', {
+			'attribute': 1,
+			'baseIndent': 1,
+			'closeBracket': 0,
+			'alignAttributesVertically': true,
+			'ignores': []
+		}],
+		'vue/max-attributes-per-line': ['error', {
+			'singleline': 4,
+			'multiline': {
+			  'max': 1,
+			  'allowFirstLine': false
+			}
+		}],
+		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		quotes: [ 'error', 'single' ],
+		semi: [ 'error', 'always' ]
+	}
 };
