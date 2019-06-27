2beaad1a9dd8c19089f0dc8f843cec1a87b849b0
wesdoyle <wesdoyle08@gmail.com>
2019-06-27 15:50:05 -0500

    Use babel, jest

---
+++ eaasi-client-web-api/test/models/alternateName.js
@@ -0,0 +1,18 @@
+import { assert } from 'chai';
+import alternateName from '../../models/alternateName';
+
+import factories from '../factories';
+import truncate from '../truncate';
+
+describe('alternateName model', () => {
+    let name = alternateName;
+
+    beforeEach(async () => {
+        await truncate();
+        name = await factories.alternateName();
+    });
+
+    it('should generate an alternateName from the factory', async () => {
+        assert.isOk(alternateName.alternateNameID);
+    });
+});
