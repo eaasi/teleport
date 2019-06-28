6a84cf8c988637e875060267fb49f35afc81859a
wesdoyle <wesdoyle08@gmail.com>
2019-06-28 16:31:32 -0500

    Work on initial migration

--- eaasi-client-web-api/src/data_access/migrations/20190628194802-initial-migration.js
+++ eaasi-client-web-api/src/data_access/migrations/20190628194802-initial-migration.js
@@ -106,6 +106,205 @@ module.exports = {
 
 				queryInterface.createTable('displayResolution', {
 				}),
+
+				queryInterface.createTable('file', {
+				}),
+
+				queryInterface.createTable('fileExtension', {
+				}),
+				queryInterface.createTable('fileFormat', {
+				}),
+				queryInterface.createTable('fileFormat_has_fileExtension', {
+				}),
+				queryInterface.createTable('fileSystem', {
+				}),
+				queryInterface.createTable('formatImplementation', {
+				}),
+				queryInterface.createTable('formatImplementation_includes_fileFormat', {
+				}),
+				queryInterface.createTable('formatOperation', {
+				}),
+
+				queryInterface.createTable('gpuDevice', {
+				}),
+				queryInterface.createTable('gpuDevice_has_displayInterface', {
+				}),
+				queryInterface.createTable('gpuDevice_has_driverSoftware', {
+				}),
+				queryInterface.createTable('gpuDevice_has_machineInterface', {
+				}),
+
+				queryInterface.createTable('keyboardDevice', {
+				}),
+				queryInterface.createTable('keyboardDevice_has_driverSoftware', {
+				}),
+				queryInterface.createTable('keyboardDevice_has_language', {
+				}),
+				queryInterface.createTable('keyboardDevice_has_machineInterfaceID', {
+				}),
+				queryInterface.createTable('keyboardLayout', {
+				}),
+				queryInterface.createTable('machineType', {
+				}),
+				queryInterface.createTable('mountFormat', {
+				}),
+				queryInterface.createTable('networkDevice', {
+				}),
+				queryInterface.createTable('networkDevice_has_driverSoftware', {
+				}),
+				queryInterface.createTable('networkDevice_has_machineInterface', {
+				}),
+				queryInterface.createTable('networkService', {
+				}),
+
+				queryInterface.createTable('objectEnvironment', {
+				}),
+				queryInterface.createTable('objectEnvironment_has_event', {
+				}),
+				queryInterface.createTable('objectFileOperation', {
+				}),
+
+				queryInterface.createTable('osVersion', {
+				}),
+				queryInterface.createTable('osVersion_colorDepthSettings', {
+				}),
+				queryInterface.createTable('osVersion_displayResolutionSettings', {
+				}),
+				queryInterface.createTable('osVersion_has_alternateID', {
+				}),
+				queryInterface.createTable('osVersion_has_developer', {
+				}),
+				queryInterface.createTable('osVersion_has_programmingLanguage', {
+				}),
+				queryInterface.createTable('osVersion_has_softwareLicense', {
+				}),
+				queryInterface.createTable('osVersion_includes_softwareVersion', {
+				}),
+				queryInterface.createTable('osVersion_isCompatibleWith_configuredMachine', {
+				}),
+
+				queryInterface.createTable('osVersion_keyboardLanguageSettings', {
+				}),
+				queryInterface.createTable('osVersion_keyboardLayoutSettings', {
+				}),
+				queryInterface.createTable('osVersion_keyboardSettings', {
+				}),
+
+				queryInterface.createTable('osVersion_languageSettings', {
+				}),
+				queryInterface.createTable('osVersion_regionSettings', {
+				}),
+				queryInterface.createTable('osVersion_timezoneSettings', {
+				}),
+
+				queryInterface.createTable('pointerDevice', {
+				}),
+				queryInterface.createTable('pointerDevice_has_driverSoftware', {
+				}),
+				queryInterface.createTable('pointerDevice_has_machineInterface', {
+				}),
+				queryInterface.createTable('pointerDeviceType', {
+				}),
+
+				queryInterface.createTable('programmingLanguage', {
+				}),
+				queryInterface.createTable('readWriteStatus', {
+				}),
+				queryInterface.createTable('region', {
+				}),
+				queryInterface.createTable('softwareEnvironment', {
+				}),
+				queryInterface.createTable('softwareEnvironment_has_diskImage', {
+				}),
+				queryInterface.createTable('softwareEnvironment_hasPart_configuredSoftware', {
+				}),
+				queryInterface.createTable('softwareFamily_hasPart_softwareProduct', {
+				}),
+				queryInterface.createTable('softwareFamilyVersion_hasPart_softwareVersion', {
+				}),
+				queryInterface.createTable('softwareLicense', {
+				}),
+				queryInterface.createTable('softwareObject', {
+				}),
+				queryInterface.createTable('softwareObject_has_alternateID', {
+				}),
+				queryInterface.createTable('softwareObject_has_event', {
+				}),
+				queryInterface.createTable('softwareObject_has_objectFile', {
+				}),
+				queryInterface.createTable('softwareObject_isManifestationOf_osVersion', {
+				}),
+				queryInterface.createTable('softwareObject_isManifestationOf_softwareVersion', {
+				}),
+				queryInterface.createTable('softwareObjectFile_has_objectFileOperation', {
+				}),
+
+
+				queryInterface.createTable('softwareProduct', {
+				}),
+
+				queryInterface.createTable('softwareProduct_has_alternateName', {
+				}),
+				queryInterface.createTable('softwareProduct_has_softwareType', {
+				}),
+				queryInterface.createTable('softwareType', {
+				}),
+				queryInterface.createTable('softwareVersion', {
+				}),
+				queryInterface.createTable('softwareVersion_has_alternateID', {
+				}),
+				queryInterface.createTable('softwareVersion_has_developer', {
+				}),
+				queryInterface.createTable('softwareVersion_has_formatImplementation', {
+				}),
+				queryInterface.createTable('softwareVersion_has_programmingLanguage', {
+				}),
+				queryInterface.createTable('softwareVersion_has_softwareLicense', {
+				}),
+				queryInterface.createTable('softwareVersion_isCompatibleWith_computingEnvironment', {
+				}),
+				queryInterface.createTable('softwareVersion_languageSettings', {
+				}),
+
+
+				queryInterface.createTable('storageDevice', {
+				}),
+				queryInterface.createTable('storageDevice_has_driverSoftware', {
+				}),
+				queryInterface.createTable('storageDevice_has_machineInterface', {
+				}),
+				queryInterface.createTable('storageDeviceType', {
+				}),
+
+
+				queryInterface.createTable('systemRequirements', {
+				}),
+				queryInterface.createTable('systemRequirements_includes_audioDevice', {
+				}),
+				queryInterface.createTable('systemRequirements_includes_cpuArchitecture', {
+				}),
+				queryInterface.createTable('systemRequirements_includes_gpuDevice', {
+				}),
+				queryInterface.createTable('systemRequirements_includes_machineType', {
+				}),
+				queryInterface.createTable('systemRequirements_includes_osVersion', {
+				}),
+				queryInterface.createTable('systemRequirements_includes_pointerDeviceType', {
+				}),
+				queryInterface.createTable('systemRequirements_includes_softwareVersion', {
+				}),
+				queryInterface.createTable('systemRequirements_includes_storageDeviceType', {
+				}),
+				queryInterface.createTable('timezone', {
+				}),
+				queryInterface.createTable('timezone_has_timezoneName', {
+				}),
+				queryInterface.createTable('timezoneName', {
+				}),
+				queryInterface.createTable('userInformation', {
+				}),
+				queryInterface.createTable('user', {
+				}),
             ]
         )
     },
