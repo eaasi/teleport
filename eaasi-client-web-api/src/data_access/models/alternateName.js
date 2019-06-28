d8aa9bff53dbc901288ce628917a105155191387
wesdoyle <wesdoyle08@gmail.com>
2019-06-28 14:37:19 -0500

    Fix model error

--- eaasi-client-web-api/src/data_access/models/alternateName.js
+++ eaasi-client-web-api/src/data_access/models/alternateName.js
@@ -1,15 +1,25 @@
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    createdAt: {
-      type: DataTypes.DATE,
-      allowNull: false
-    },
-    updatedAt: {
-      type: DataTypes.DATE,
-      allowNull: false
-    }
-  }, {
-    tableName: 'alternateName'
-  });
+module.exports = function(sequelize, DataTypes) {
+    return sequelize.define('alternateName', {
+        alternateNameID: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        alternateName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        updatedAt: {
+            type: DataTypes.DATE,
+            allowNull: false
+        }
+    }, {
+        tableName: 'alternateName'
+    });
 };
+
