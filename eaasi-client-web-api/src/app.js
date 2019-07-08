a5f2455882e58e4766fc04a36deffc141a36df0b
wesdoyle <wesdoyle08@gmail.com>
2019-07-08 16:18:35 -0500

    Redirect to v1 route

--- eaasi-client-web-api/src/app.js
+++ eaasi-client-web-api/src/app.js
@@ -20,6 +20,10 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/api', require('./routes'))
 
+app.use('/', function(req, res) {
+  res.redirect('/api/v1.0');
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
