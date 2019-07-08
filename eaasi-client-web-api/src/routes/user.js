943220be3edd4c2627ba76c2d7a0f876f4796bb9
wesdoyle <wesdoyle08@gmail.com>
2019-07-08 15:21:17 -0500

    ESLintify

--- eaasi-client-web-api/src/routes/user.js
+++ eaasi-client-web-api/src/routes/user.js
@@ -4,10 +4,10 @@ const router = express.Router();
 const UserController = require('../controllers/UserController');
 const controller = new UserController();
 
-router.get("/", (req, res) => controller.getAll(req, res));
-router.get("/:id", (req, res) => controller.get(req, res));
-router.post("/", (req, res) => controller.create(req, res));
-router.put("/:id", (req, res) => controller.update(req, res));
-router.delete("/:id", (req, res) => controller.delete(req, res));
+router.get('/', (req, res) => controller.getAll(req, res));
+router.get('/:id', (req, res) => controller.get(req, res));
+router.post('/', (req, res) => controller.create(req, res));
+router.put('/:id', (req, res) => controller.update(req, res));
+router.delete('/:id', (req, res) => controller.delete(req, res));
 
 module.exports = router;
