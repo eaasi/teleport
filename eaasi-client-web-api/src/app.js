28cce0fe5848aa4c77ed8fe47243c758a13ce062
wesdoyle <wesdoyle08@gmail.com>
2019-07-08 16:09:21 -0500

    Update routes

--- eaasi-client-web-api/src/app.js
+++ eaasi-client-web-api/src/app.js
@@ -1,11 +1,9 @@
 const createError = require('http-errors');
 const express = require('express');
+const helmet = require('helmet');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
-const index = require('./routes/index');
-const user = require('./routes/user');
 
 const app = express();
 
@@ -13,14 +11,14 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.use('/', index);
-app.use('/user', user);
+app.use('/api', require('./routes'))
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- eaasi-client-web-api/src/routes/index.js
+++ eaasi-client-web-api/src/routes/index.js
@@ -1,9 +1,7 @@
 const express = require('express');
+
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-	res.render('index', { title: 'EaaSI Internal Web API' });
-});
+router.use('/v1.0', require('./v1.0'))
 
 module.exports = router;
