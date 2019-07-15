397644734f9f86fed174071b117d2496f3d6e3f5
wesdoyle <wesdoyle08@gmail.com>
2019-07-15 07:55:34 -0500

    Clean up

--- eaasi-client-web-api/test/unit/app.spec.js
+++ eaasi-client-web-api/test/unit/app.spec.js
@@ -1,16 +1,16 @@
 const request = require('supertest')
-const appSpec = require('../../src/app')
+const app = require('../../src/app')
 
 
 describe('The root web API path', () => {
 	it('should provide a 302 redirect response to GET', () => {
-		return request(appSpec).get("/").then(response => {
+		return request(app).get("/").then(response => {
 			expect(response.statusCode).toBe(302);
 		})
 	});
 
 	it('should send redirect to /api/v1.0 in headers GET', () => {
-		return request(appSpec).get("/").then(response => {
+		return request(app).get("/").then(response => {
 			expect(response.headers.location).toBe('/api/v1.0');
 		})
 	});
