f017b346cfbf0e44504986078f82c3b0ba2033bf
wesdoyle <wesdoyle08@gmail.com>
2019-07-25 15:01:01 -0500

    Add ErrorResponse class

--- eaasi-client-web-api/src/utils/json_response_helpers.js
+++ eaasi-client-web-api/src/utils/json_response_helpers.js
@@ -2,11 +2,24 @@
  * Standard Error Responses for web API Requests
  */
 
+import {NOT_FOUND} from "../../dist/http_helpers";
+
+class ErrorResponse {
+	constructor(httpCode, message) {
+		this.message = {
+			status: httpCode,
+			message: message
+		}
+	}
+}
+
 
 /**
  * Resource not found response
  * @type {{}}
  */
-const Error404Response = {
-
+function build_404_response(resource_name) {
+	const message =
+		`The resource ${resource_name} was not found at this location.`;
+	return new ErrorResponse(NOT_FOUND, message).message
 }
