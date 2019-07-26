fdbee3bb9b0c8a710642d2981dbf9b58e7a0a2c1
wesdoyle <wesdoyle08@gmail.com>
2019-07-26 14:53:04 -0500

    Add unit test for getAll return

--- eaasi-client-web-api/test/helpers/doubles/sequelize-model-fake.js
+++ eaasi-client-web-api/test/helpers/doubles/sequelize-model-fake.js
@@ -5,29 +5,29 @@ export default class SequelizeModelFake {
 
 	constructor(modelName, fakeResultCount) {
 		this.modelName = modelName
+		this.rows = []
 		this.resultCount = fakeResultCount || 0
 		this.findAndCountAll_callCount = 0;
 		this.findAll_callCount = 0;
+
+		if (this.resultCount > 0) {
+			for (let i=0; i < this.resultCount; i++) {
+				this.rows.push({id: i})
+			}
+		}
 	}
 
 	async findAndCountAll() {
 		this.findAndCountAll_callCount += 1;
-		let rows = []
-
-		if (this.resultCount > 0) {
-			for (let i=0; i < this.resultCount; i++) {
-				rows.push({id: i})
-			}
-		}
 
 		return await {
-			count: this.resultCount,
-			rows: rows
+			count: this.rows.length,
+			rows: this.rows
 		};
 	}
 
 	async findAll() {
 		this.findAll_callCount += 1;
-		return await []
+		return await this.rows
 	};
 }
--- eaasi-client-web-api/test/unit/services/base/api-service.spec.js
+++ eaasi-client-web-api/test/unit/services/base/api-service.spec.js
@@ -5,15 +5,15 @@ import SequelizeModelFake from "../../../helpers/doubles/sequelize-model-fake"
 describe("API Service", () => {
 	it("should assign domain model via ctor",
 		() => {
-			let modelSpy = new SequelizeModelFake("fakeModel");
-			let sut = new ApiService(modelSpy);
+			let modelFake = new SequelizeModelFake("fakeModel");
+			let sut = new ApiService(modelFake);
 			expect(sut.model).toBeInstanceOf(SequelizeModelFake);
 		});
 
 	it("should have default MAX_GET_PAGE_SIZE of 100",
 		() => {
-			let modelSpy = new SequelizeModelFake("fakeModel");
-			let sut = new ApiService(modelSpy);
+			let modelFake = new SequelizeModelFake("fakeModel");
+			let sut = new ApiService(modelFake);
 			expect(sut.MAX_GET_ALL_PAGE_SIZE).toBe(100);
 		});
 
@@ -27,33 +27,43 @@ describe("API Service", () => {
 	`
 	('should set custom MAX_GET_ALL_PAGE_SIZE with input ($input) via instance method',
 		({input, expected}) => {
-			let modelSpy = new SequelizeModelFake("fakeModel");
-			let sut = new ApiService(modelSpy);
+			let modelFake = new SequelizeModelFake("fakeModel");
+			let sut = new ApiService(modelFake);
 			sut.setMaxPaginationValue(input);
 			expect(sut.MAX_GET_ALL_PAGE_SIZE).toBe(expected);
 		});
 
 	it("should called findAndCountAll() once when getting all model instances",
 		async () => {
-			let modelSpy = new SequelizeModelFake("fakeModel");
-			let sut = new ApiService(modelSpy);
+			let modelFake = new SequelizeModelFake("fakeModel");
+			let sut = new ApiService(modelFake);
 			await sut.getAll(10, 1, 'id');
 			expect(sut.model.findAndCountAll_callCount).toBe(1);
 		});
 
 	it("should called findAll() once when getting all model instances",
 		async () => {
-			let modelSpy = new SequelizeModelFake("fakeModel");
-			let sut = new ApiService(modelSpy);
+			let modelFake = new SequelizeModelFake("fakeModel");
+			let sut = new ApiService(modelFake);
 			await sut.getAll('foo', 'bar', 'baz');
 			expect(sut.model.findAll_callCount).toBe(1);
 		});
 
-	it("should return correct length of results given page",
+	it("should respond with hasError: false with in a successful response object",
 		async () => {
-			let modelSpy = new SequelizeModelFake("fakeModel", 20);
-			let sut = new ApiService(modelSpy);
-			await sut.getAll('foo', 'bar', 'baz');
-			expect(sut.model.findAll_callCount).toBe(1);
+			let modelFake = new SequelizeModelFake("fakeModel");
+			let sut = new ApiService(modelFake);
+			const response = await sut.getAll('foo', 'bar', 'baz');
+			expect(response.hasError).toBe(false);
+		});
+
+	it("should return correct length of results given limit and page params",
+		async () => {
+			let modelFake = new SequelizeModelFake("fakeModel", 20);
+			let sut = new ApiService(modelFake);
+
+			const response = await sut.getAll(100, 1);
+
+			expect(response.result.count).toBe(20);
 		});
 })
