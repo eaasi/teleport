65b31556495b50ab11c06e61501f2fc369bebeb3
wesdoyle <wesdoyle08@gmail.com>
2019-07-29 09:51:25 -0500

    Clean up

--- eaasi-client-web-api/test/unit/services/base/api-service.spec.js
+++ eaasi-client-web-api/test/unit/services/base/api-service.spec.js
@@ -223,9 +223,14 @@ describe("API Service", () => {
 		expect(res).toStrictEqual({hasError: true, error: "notFound"});
 	})
 
-
-
 	// Pagination Tests
+
+	it("should have default MAX_GET_PAGE_SIZE of 100",
+		() => {
+			let modelFake = new SequelizeModelFake("fakeModel");
+			let sut = new ApiService(modelFake);
+			expect(sut.MAX_GET_ALL_PAGE_SIZE).toBe(100);
+		});
 
 	it.each`
 	limit   | page | tableSize | expected
@@ -244,32 +249,6 @@ describe("API Service", () => {
 			let sut = new ApiService(modelFake);
 			const response = await sut.getAll(limit, page);
 			expect(response.result.count).toBe(expected);
-		});
-
-	it.each`
-	limit   | page | tableSize | expected
-	${0}    | ${0} | ${0}      | ${0}    // 0th page always returns 0 results
-	${1}    | ${0} | ${0}      | ${0}    // 0th page always returns 0 results
-	${1}    | ${1} | ${0}      | ${0}    // Take 1 from page 1 with empty table returns 0 results
-	${1}    | ${1} | ${1}      | ${1}    // Take 1 from page 1 with 1 result in table returns 1 result
-	${2}    | ${1} | ${1}      | ${1}    // Take 2 from page 1 with 1 result in table returns 1 result
-	${1}    | ${2} | ${1}      | ${0}    // Take 1 from page 2 with 1 result in table returns 0 results
-	${100}  | ${1} | ${250}    | ${100}  // Take 100 from page 1 with 250 results in table returns 100 results
-	${100}  | ${2} | ${250}    | ${100}  // Take 100 from page 2 with 250 results in table returns 100 results
-	${100}  | ${3} | ${250}    | ${50}   // Take 100 from page 3 with 250 results in table returns 50 results
-	`("should return $expected results on a page given limit: $limit, page: $page, and table size: $tableSize",
-		async ({limit, page, tableSize, expected}) => {
-			let modelFake = new SequelizeModelFake("fakeModel", tableSize);
-			let sut = new ApiService(modelFake);
-			const response = await sut.getAll(limit, page);
-			expect(response.result.result.length).toBe(expected);
-		});
-
-	it("should have default MAX_GET_PAGE_SIZE of 100",
-		() => {
-			let modelFake = new SequelizeModelFake("fakeModel");
-			let sut = new ApiService(modelFake);
-			expect(sut.MAX_GET_ALL_PAGE_SIZE).toBe(100);
 		});
 
 	it.each`
