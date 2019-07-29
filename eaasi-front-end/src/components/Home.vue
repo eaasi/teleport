3ac9815d6bc5cce4b0a4463bdcf0b1f856b03745
Brandon Wiemann <brandon@portalmedia.com>
2019-07-29 15:33:15 -0500

    Home component test cleanup

--- eaasi-front-end/src/components/Home.vue
+++ eaasi-front-end/src/components/Home.vue
@@ -1,50 +1,15 @@
 <template>
 	<div class="home">
 		<h1>(Eaasi Goes Here)</h1>
-		<big-button
-			icon="bolt"
-			label="Big Button"
-			sublabel="Explanation of button here"
-			info="Additional information could go here in the future."
-			size="sm"
-		/>
 	</div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import { IEaasiTab } from '@/types/Navigation';
-import BigButton from '@/components/global/BigButton.vue';
-@Component({
-	components: {
-		BigButton
-	}
-})
-export default class Home extends Vue {
-	clickedButton() : void {
-		console.log('Button was clicked!');
-	}
 
-	activeTab: string = 'Tab Option 1';
-	toggleOn: boolean = false;
-	pageNum: number = 1;
-	showModal: boolean = false;
-
-	tabs: Array<IEaasiTab> = [
-		{
-			label: 'Tab Option 1',
-			icon: null
-		},
-		{
-			label: 'Tab Option 2',
-			icon: null
-		},
-		{
-			label: 'Tab Option 3',
-			icon: null
-		}
-	]
-}
+@Component
+export default class Home extends Vue {}
 </script>
 
 <style>
