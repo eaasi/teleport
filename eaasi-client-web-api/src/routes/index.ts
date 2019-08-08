0f952e6dfbfcaa52616264c9a2c2c67a67f70504
Wes Doyle <wesdoyle08@gmail.com>
2019-08-08 15:46:59 +0000

    138 query string validation

--- eaasi-client-web-api/src/routes/index.ts
+++ eaasi-client-web-api/src/routes/index.ts
@@ -4,11 +4,16 @@ const router = express.Router();
 
 router.use('/eaasi-user', require('./eaasi-user'))
 router.use('/eaasi-role', require('./eaasi-role'))
-router.use('/', require('./eaasi-auth'))
 
-/* GET home page. */
-router.get('/', function(req: express.Request, res: express.Response, next: express.NextFunction) {
-	res.render('index', { title: 'EaaSI Internal Web API (version 1.0)' });
+router.get('/', function(req: express.Request, res: express.Response) {
+	res.render('index',
+		{
+			title: 'EaaSI Internal Web API (version 1.0)'
+		});
+});
+
+router.get('*', function(req: express.Request, res: express.Response) {
+	res.redirect('/')
 });
 
 module.exports = router;
