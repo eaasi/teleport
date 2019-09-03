c173379bd7ae0d988b3cee9a6bb307800c762535
Brandon Wiemann <brandon@portalmedia.com>
2019-09-03 11:54:38 -0500

    WIP shib spike

--- eaasi-web-api/src/middleware/passport.js
+++ eaasi-web-api/src/middleware/passport.js
@@ -1,5 +1,15 @@
 import passport from 'passport';
 import jwt from 'passport-jwt';
+import { Strategy as SamlStrategy } from 'passport-saml';
+import fs from 'fs';
+
+import { cert } from './public-cert';
+
+const shibCert = fs.readFileSync('../../shibboleth-idp/certificate.pem', {
+	encoding: 'utf8'
+}).toString();
+console.log(shibCert);
+console.log(cert);
 
 // TODO: Get the user from the db?
 passport.use(new jwt.Strategy({
@@ -7,4 +17,18 @@ passport.use(new jwt.Strategy({
 	secretOrKey   : process.env.JWT_SECRET
 }, (jwtPayload, done) => {
 	done(null, jwtPayload);
+}));
+
+let samlConfig = {
+	ID: 'http://localhost:8081',
+	callbackUrl: 'http://localhost:8081/auth/login',
+	entryPoint: 'https://localhost:443/idp',
+	issuer: 'passport-saml',
+	// privateCert: shibCert,
+	cert
+};
+
+passport.use(new SamlStrategy(samlConfig, function(profile, done) {
+	console.log('PROFILE', JSON.stringify(profile, null, 4));
+	done();
 }));
--- eaasi-web-api/src/routes/eaasi-auth.ts
+++ eaasi-web-api/src/routes/eaasi-auth.ts
@@ -1,11 +1,12 @@
 import express from "express";
+import passport from 'passport';
 
 const router = express.Router();
 
 const EaasiAuthController = require('../controllers/EaasiAuthController');
 const controller = new EaasiAuthController();
 
-router.post('/login', controller.login);
+router.post('/login', passport.authenticate('saml'), controller.login);
 router.delete('/logout', controller.logout);
 router.post('/refresh', controller.refresh);
 
