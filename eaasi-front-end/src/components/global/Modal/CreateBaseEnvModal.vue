117a5518905314e0c27e43f79c8e35c3b6f736c0
wesdoyle <wesdoyle08@gmail.com>
2019-09-09 13:22:05 -0500

    Create CreateBaseEnvModal

---
+++ eaasi-front-end/src/components/global/Modal/CreateBaseEnvModal.vue
@@ -0,0 +1,37 @@
+<template>
+	<modal @close="$emit('close')" :size="size">
+		<div class="eaasi-info-modal-title" slot="header">
+			<h2 v-if="title">Create Base Environment</h2>
+		</div>
+		<slot></slot>
+		<div class="eaasi-info-modal-buttons" slot="footer">
+			<div class="flex-row justify-end">
+				<slot name="buttons">
+					<div class="justify-end buttons-right">
+						<ui-button @click="$emit('close')" secondary>Cancel</ui-button>
+					</div>
+				</slot>
+			</div>
+		</div>
+	</modal>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { Component, Prop } from 'vue-property-decorator';
+import { Modal, UiButton } from '@/components/global';
+
+@Component({
+	name: 'CreateBaseEnvModal',
+	components: {
+		Modal,
+		UiButton
+	}
+})
+export default class CreateBaseEnvModal extends Vue {
+}
+
+</script>
+
+<style lang="scss">
+</style>
