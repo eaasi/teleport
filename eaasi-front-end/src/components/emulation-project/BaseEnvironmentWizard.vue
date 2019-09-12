7489e1cb6649fbb9abb656ce8b4712d56c84af38
wesdoyle <wesdoyle08@gmail.com>
2019-09-12 17:02:19 -0500

    Miscss

--- eaasi-front-end/src/components/emulation-project/BaseEnvironmentWizard.vue
+++ eaasi-front-end/src/components/emulation-project/BaseEnvironmentWizard.vue
@@ -1,12 +1,13 @@
 <template>
 	<div class="base-env-wiz-container">
 		<div class="base-env-wiz-opt">
-			<options-box optionName="Find a Base">
+			<options-box option-name="Find a Base" >
+				Find an existing base environment.
 			</options-box>
 		</div>
 		<div class="base-env-wiz-opt">
-			<options-box optionName="Start from Scratch" header="Less Common">
-				Start from scratch
+			<options-box option-name="Start from Scratch" header="Less Common">
+				Use a system template with no configured operating system or software.
 			</options-box>
 		</div>
 	</div>
@@ -25,11 +26,14 @@ export default class BaseEnvironmentWizard extends Vue {
 
 <style lang="scss">
 	.base-env-wiz-container {
+		align-items: stretch;
 		display: flex;
 		flex-direction: row;
 	}
 
 	.base-env-wiz-opt {
-		margin: auto;
+		align-self: flex-start;
+		margin: 0 1.8rem;
+		min-height: 32rem;
 	}
 </style>
