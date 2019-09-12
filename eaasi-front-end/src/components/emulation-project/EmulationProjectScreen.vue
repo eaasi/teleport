c6a0799abf87f187dccba32324daf23c6a886100
wesdoyle <wesdoyle08@gmail.com>
2019-09-12 14:41:07 -0500

    Lint

--- eaasi-front-end/src/components/emulation-project/EmulationProjectScreen.vue
+++ eaasi-front-end/src/components/emulation-project/EmulationProjectScreen.vue
@@ -2,13 +2,13 @@
 	<div>
 		<view-header title="Emulation Project" />
 		<div>
-			<section-heading title="Base Environment"/>
+			<section-heading title="Base Environment" />
 		</div>
 		<div>
-			<section-heading title="Software Resources"/>
+			<section-heading title="Software Resources" />
 		</div>
 		<div>
-			<section-heading title="Content Resources"/>
+			<section-heading title="Content Resources" />
 		</div>
 	</div>
 </template>
