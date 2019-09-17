bcb3b42ef7b0e79d0dcc19510eacb76b331a8f63
wesdoyle <wesdoyle08@gmail.com>
2019-09-17 15:22:41 -0500

    Add a test

--- eaasi-front-end/tests/unit/components/global/Alert.spec.ts
+++ eaasi-front-end/tests/unit/components/global/Alert.spec.ts
@@ -1,11 +1,11 @@
 import { shallowMount } from '@vue/test-utils';
-import { AlertCard } from '@/components/global';
+import Alert from '@/components/global/Alert/Alert.vue';
 
-describe('AlertCard.vue', () => {
-	it('Error alert card shows triangle warning icon', () => {
-		const wrapper = shallowMount(AlertCard, {
+describe('Alert.vue', () => {
+	it('Displays error icon when alertType is error', () => {
+		const wrapper = shallowMount(Alert, {
 			propsData: {
-				type: 'error'
+				alertType: 'error'
 			},
 		});
 		expect(wrapper.find('.fa-exclamation-triangle').exists()).toBe(true);
@@ -13,49 +13,48 @@ describe('AlertCard.vue', () => {
 		expect(wrapper.find('.fa-check-circle').exists()).toBe(false);
 	});
 
-	it('Warning alert card shows triangle warning icon', () => {
-		const wrapper = shallowMount(AlertCard, {
+	it('Displays info icon when alertType is info', () => {
+		const wrapper = shallowMount(Alert, {
 			propsData: {
-				type: 'warning'
+				alertType: 'info'
 			},
 		});
-		expect(wrapper.find('.fa-exclamation-triangle').exists()).toBe(true);
-		expect(wrapper.find('.fa-info-circle').exists()).toBe(false);
-		expect(wrapper.find('.fa-check-circle').exists()).toBe(false);
-	});
-
-	it('Success alert card shows check circle icon', () => {
-		const wrapper = shallowMount(AlertCard, {
-			propsData: {
-				type: 'success'
-			},
-		});
-		expect(wrapper.find('.fa-check-circle').exists()).toBe(true);
 		expect(wrapper.find('.fa-exclamation-triangle').exists()).toBe(false);
-		expect(wrapper.find('.fa-info-circle').exists()).toBe(false);
-	});
-
-	it('Info alert card shows info circle icon', () => {
-		const wrapper = shallowMount(AlertCard, {
-			propsData: {
-				type: 'info'
-			},
-		});
 		expect(wrapper.find('.fa-info-circle').exists()).toBe(true);
-		expect(wrapper.find('.fa-exclamation-triangle').exists()).toBe(false);
 		expect(wrapper.find('.fa-check-circle').exists()).toBe(false);
 	});
 
-	it('Renders text in slot', () => {
-		const wrapper = shallowMount(AlertCard, {
+	it('Displays success icon when alertType is success', () => {
+		const wrapper = shallowMount(Alert, {
 			propsData: {
-				type: 'info'
+				alertType: 'success'
+			},
+		});
+		expect(wrapper.find('.fa-exclamation-triangle').exists()).toBe(false);
+		expect(wrapper.find('.fa-info-circle').exists()).toBe(false);
+		expect(wrapper.find('.fa-check-circle').exists()).toBe(true);
+	});
+
+	it('Displays warning icon when alertType is warning', () => {
+		const wrapper = shallowMount(Alert, {
+			propsData: {
+				alertType: 'error'
+			},
+		});
+		expect(wrapper.find('.fa-exclamation-triangle').exists()).toBe(true);
+		expect(wrapper.find('.fa-info-circle').exists()).toBe(false);
+		expect(wrapper.find('.fa-check-circle').exists()).toBe(false);
+	});
+
+	it('Renders alert text passed into slot', () => {
+		const wrapper = shallowMount(Alert, {
+			propsData: {
+				alertType: 'error'
 			},
 			slots: {
-				default: 'crucial information'
+				default: 'wherever you go'
 			}
 		});
-		expect(wrapper.find('.content').text())
-			.toBe('crucial information');
+		expect(wrapper.find('.content').text()).toContain('wherever you go');
 	});
 });
