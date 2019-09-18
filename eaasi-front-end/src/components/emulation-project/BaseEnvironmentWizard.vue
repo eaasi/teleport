c66f8425a47bc6d2477c25556c126ee5612a4a84
Brandon Wiemann <brandon@portalmedia.com>
2019-09-18 10:33:38 -0500

    adds flexbox grid,  updates alert components

--- eaasi-front-end/src/components/emulation-project/BaseEnvironmentWizard.vue
+++ eaasi-front-end/src/components/emulation-project/BaseEnvironmentWizard.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="base-env-wiz-container">
-		<div class="base-env-wiz-left">
-			<div class="base-env-wiz-opt">
+		<div class="row">
+			<div class="col-md-4">
 				<options-box option-name="Find a Base" header="Recommended" style="height: 100%;">
 					Find an existing base environment.
 					<template v-slot:footer>
@@ -22,7 +22,7 @@
 				</options-box>
 			</div>
 
-			<div class="base-env-wiz-opt">
+			<div class="col-md-4">
 				<options-box option-name="Start from Scratch" header="Less Common" style="height: 100%;">
 					Use a system template with no configured operating system or software.
 					<template v-slot:footer>
@@ -35,16 +35,15 @@
 					</template>
 				</options-box>
 			</div>
-		</div>
 
-		<div class="base-env-wiz-right">
-			<div v-if="showTips">
-				<tips-card>
-					You will need at least a base environment to run an emulation project
-				</tips-card>
+			<div class="col-md-4">
+				<div v-if="showTips">
+					<tips-card>
+						You will need at least a base environment to run an emulation project
+					</tips-card>
+				</div>
 			</div>
 		</div>
-
 		<create-base-env-modal v-if="isChooseTemplateOpen" @close="closeChooseTemplate" />
 	</div>
 </template>
@@ -96,26 +95,4 @@ export default class BaseEnvironmentWizard extends Vue {
 </script>
 
 <style lang="scss">
-	.base-env-wiz-container {
-		align-items: stretch;
-		display: flex;
-		flex-direction: row;
-	}
-
-	.base-env-wiz-left {
-		display: flex;
-		min-width: 50vw;
-	}
-
-	.base-env-wiz-right {
-		display: flex;
-		justify-content: flex-start;
-	}
-
-	.base-env-wiz-opt {
-		align-content: stretch;
-		flex-grow: 1;
-		margin-right: 1rem;
-		padding: 1.2rem 0;
-	}
 </style>
