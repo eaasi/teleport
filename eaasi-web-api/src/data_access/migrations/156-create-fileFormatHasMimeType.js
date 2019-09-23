ee2c6c6c5873465fb8b7ce3180d4cc2fc8458a35
wesdoyle <wesdoyle08@gmail.com>
2019-09-23 08:23:39 -0500

    create ff_has_mimeType

---
+++ eaasi-web-api/src/data_access/migrations/156-create-fileFormatHasMimeType.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const Sq = require('sequelize');
+
+module.exports = {
+	up: (queryInterface, Sequelize) => {
+		return queryInterface.createTable('fileFormat_has_mimeType', {
+			createdAt: Sq.DATE,
+			updatedAt: Sq.DATE,
+			fileFormat_fileFormatID: {
+				type: Sq.STRING,
+				allowNull: false,
+				references: {
+					model: 'fileFormat',
+					key: 'fileFormatID'
+				}
+			},
+			fileFormat_mimeTypeLabel: {
+				type: Sq.STRING,
+				allowNull: false,
+				references: {
+					model: 'mimeType',
+					key: 'mimeTypeID'
+				}
+			}
+		});
+	},
+	down: (queryInterface, Sequelize) => {
+		return queryInterface.dropTable('fileFormat_has_mimeType');
+	}
+};
