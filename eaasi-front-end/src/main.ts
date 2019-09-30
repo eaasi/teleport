028cfddf399e05ea231f931a8034b778b7067124
wesdoyle <wesdoyle08@gmail.com>
2019-09-30 14:39:00 -0500

    Temp use raw loader

--- eaasi-front-end/src/main.ts
+++ eaasi-front-end/src/main.ts
@@ -6,16 +6,21 @@ import store from './store';
 import EaasiVueConfig from '@/plugins/EaasiVueConfig';
 import GlobalComponents from '@/components/global';
 import { appendScript } from '@/utils/functions';
+import guacamolejs from '!!raw-loader!../../eaas-client/guacamole/guacamole.js';
+import eaasclientjs from '!!raw-loader!../../eaas-client/eaas-client.js';
+
+appendScript(guacamolejs);
+appendScript(eaasclientjs);
 
 // TODO: This is to make CI pipelines succeed. Eventually these will become ES6 Modules
-try {
-	const guacamolejs = require('!!raw-loader!../../eaas-client/guacamole/guacamole.js');
-	const eaasclientjs = require('!!raw-loader!../../eaas-client/eaas-client.js');
-	appendScript(guacamolejs);
-	appendScript(eaasclientjs);
-} catch(e) {
-	console.warn('Could not import eaas-client and guacamole scripts. Please init eeas-client submodule');
-}
+// try {
+// 	const guacamolejs = require('!!raw-loader!../../eaas-client/guacamole/guacamole.js');
+// 	const eaasclientjs = require('!!raw-loader!../../eaas-client/eaas-client.js');
+// 	appendScript(guacamolejs);
+// 	appendScript(eaasclientjs);
+// } catch(e) {
+// 	console.warn('Could not import eaas-client and guacamole scripts. Please init eeas-client submodule');
+// }
 
 Vue.use(EaasiVueConfig);
 Vue.use(GlobalComponents);
