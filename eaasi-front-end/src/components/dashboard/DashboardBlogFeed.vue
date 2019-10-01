f90cd27d4a1b3cb56a5c989242658eb17ce5cb6c
wesdoyle <wesdoyle08@gmail.com>
2019-10-01 07:57:15 -0500

    Misc

--- eaasi-front-end/src/components/dashboard/DashboardBlogFeed.vue
+++ eaasi-front-end/src/components/dashboard/DashboardBlogFeed.vue
@@ -11,7 +11,7 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import RssService from "@/services/RssService";
+import RssService from '@/services/RssService';
 
 let rssService = RssService;
 
