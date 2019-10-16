2f2327a73a0d5356990ad007efc78a94eabed890
wesdoyle <wesdoyle08@gmail.com>
2019-10-16 11:29:56 -0500

    Update ReplicateImageRequest

--- eaasi-front-end/src/components/resources/models/ReplicateImageRequest.ts
+++ eaasi-front-end/src/components/resources/models/ReplicateImageRequest.ts
@@ -1,5 +1,15 @@
+/**
+ * Encapsulates a request to replicate / save an Environment
+ */
 class ReplicateImageRequest {
+	/**
+	 * Destination archive
+	 */
 	private destArchive: string;
+
+	/**
+	 * List of environmentIds to replicate
+	 */
 	private replicateList: string[];
 
 	constructor(replicateList: string[], destArchive: string) {
@@ -8,6 +18,14 @@ class ReplicateImageRequest {
 	}
 
 	addToReplicateList(envId: string) {
-		
+		this.replicateList.push(envId);
+	}
+
+	addMultipleToReplicateList(envIds: string[]) {
+		return this.replicateList.slice().concat(envIds);
+	}
+
+	setDestArchive(destArchive: string) {
+		this.destArchive = destArchive;
 	}
 }
