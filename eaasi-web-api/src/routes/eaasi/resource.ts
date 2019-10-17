d611b3f5f0f41344857bce1f30fe272c42587513
wesdoyle <wesdoyle08@gmail.com>
2019-10-17 10:59:07 -0500

    Remove ws

--- eaasi-web-api/src/routes/eaasi/resource.ts
+++ eaasi-web-api/src/routes/eaasi/resource.ts
@@ -6,7 +6,6 @@ const controller = new ResourceController();
 
 router.get('/environment', (req, res) => controller.getEnvironment(req, res));
 router.get('/software', (req, res) => controller.getSoftwarePackageDescription(req, res));
-
 router.post('/search', (req, res) => controller.search(req, res));
 router.post('/save', (req, res) => controller.saveEnvironment(req, res));
 
