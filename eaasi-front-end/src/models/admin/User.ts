e405de2f9d8f6457b196663f53559b6da9caadcd
Andrii Kost <andrii@Andriis-MacBook-Pro.local>
2019-10-21 11:44:20 -0500

    adds optional param to constructor User

--- eaasi-front-end/src/models/admin/User.ts
+++ eaasi-front-end/src/models/admin/User.ts
@@ -18,16 +18,18 @@ export default class User implements IEaasiUser {
 	updatedAt: Date;
 	lastLogin: Date;
 
-	constructor(user: IEaasiUser) {
-		this.id = user.id;
-		this.email = user.email;
-		this.firstName = user.firstName;
-		this.lastName = user.lastName;
-		this.username = user.username;
-		this.roleId = user.roleId;
-		this.createdAt = user.createdAt;
-		this.updatedAt = user.updatedAt;
-		this.lastLogin = user.lastLogin;
+	constructor(user: IEaasiUser = null) {
+		if (user) {
+			this.id = user.id;
+			this.email = user.email;
+			this.firstName = user.firstName;
+			this.lastName = user.lastName;
+			this.username = user.username;
+			this.roleId = user.roleId;
+			this.createdAt = user.createdAt;
+			this.updatedAt = user.updatedAt;
+			this.lastLogin = user.lastLogin;
+		}
 	}
 
 	get userHasEditPermissions() {
