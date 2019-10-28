9dd32f29aacb0aee57660a12470e77a6913d3445
wesdoyle <wesdoyle08@gmail.com>
2019-10-28 10:16:59 -0500

    Update component loader

--- eaasi-front-end/src/components/global/ComponentLoader.vue
+++ eaasi-front-end/src/components/global/ComponentLoader.vue
@@ -1,28 +1,29 @@
 <template>
 	<div class="component-loader">
-	<svg
-		viewBox="0 0 38 38"
-		xmlns="http://www.w3.org/2000/svg"
-		width="48px"
-		height="48px"
-		:stroke="color"
-		class="cl-svg"
-	>
-		<g fill="none" fill-rule="evenodd">
-			<g transform="translate(1 1)" stroke-width="2">
-				<circle stroke-opacity=".25" cx="18" cy="18" r="18"/>
-				<path d="M36 18c0-9.94-8.06-18-18-18">
-					<animateTransform
-						attributeName="transform"
-						type="rotate"
-						from="0 18 18"
-						to="360 18 18"
-						dur="0.8s"
-						repeatCount="indefinite"/>
-				</path>
+		<svg
+			viewBox="0 0 38 38"
+			xmlns="http://www.w3.org/2000/svg"
+			width="48px"
+			height="48px"
+			:stroke="color"
+			class="cl-svg"
+		>
+			<g fill="none" fill-rule="evenodd">
+				<g transform="translate(1 1)" stroke-width="2">
+					<circle stroke-opacity=".25" cx="18" cy="18" r="18" />
+					<path d="M36 18c0-9.94-8.06-18-18-18">
+						<animateTransform
+							attributeName="transform"
+							type="rotate"
+							from="0 18 18"
+							to="360 18 18"
+							dur="0.8s"
+							repeatCount="indefinite"
+						/>
+					</path>
+				</g>
 			</g>
-		</g>
-	</svg>
+		</svg>
 	</div>
 </template>
 
@@ -55,8 +56,8 @@ export default class ComponentLoader extends Vue {
 		width: 100%;
 
 		.cl-svg {
-			position: absolute;
 			left: 40%;
+			position: absolute;
 			top: 32%;
 		}
 	}
