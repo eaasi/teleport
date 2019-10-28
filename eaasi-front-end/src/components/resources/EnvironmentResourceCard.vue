b769c23cf6f9f3802c56f5f6891bbe276a8026ab
wesdoyle <wesdoyle08@gmail.com>
2019-10-28 13:38:56 -0500

    Work on Resource Card

--- eaasi-front-end/src/components/resources/EnvironmentResourceCard.vue
+++ eaasi-front-end/src/components/resources/EnvironmentResourceCard.vue
@@ -1,7 +1,8 @@
 <template>
 	<selectable-card
+		v-if="cardSummary"
 		:bookmark="true"
-		:data="summary"
+		:data="cardSummary"
 		:footer="true"
 		:is-loading="isLoading"
 		@change="setActiveEnvironment"
@@ -16,126 +17,152 @@
 </template>
 
 <script lang="ts">
-import ResourceService from '@/services/ResourceService';
-import { ITag } from '@/types/Tag';
-import { resourceTypes } from '@/utils/constants';
-import Vue from 'vue';
-import { Component, Prop } from 'vue-property-decorator';
-import {IEaasiEnvironmentCardSummary, IEaasiResourceSummary, IEnvironment} from '@/types/Resource.d.ts';
+    import ResourceService from '@/services/ResourceService';
+    import {IEaasiEnvironmentCardSummary, IEaasiResourceSummary, IEnvironment} from '@/types/Resource.d.ts';
+    import {ITag} from '@/types/Tag';
+    import {resourceTypes} from '@/utils/constants';
+	import StringCleaner from '@/utils/string-cleaner';
+    import Vue from 'vue';
+    import {Component, Prop} from 'vue-property-decorator';
 
-let resourceSvc = ResourceService;
+    let resourceSvc = ResourceService;
 
-@Component({
-	name: 'EnvironmentResourceCard',
-})
-export default class EnvironmentResourceCard extends Vue {
+	@Component({
+		name: 'EnvironmentResourceCard',
+	})
+    export default class EnvironmentResourceCard extends Vue {
 
-	/* Props
-	============================================*/
-	@Prop({type: Object as () => IEnvironment, required: true})
-	readonly environment: IEnvironment
+		/* Props
+        ============================================*/
+		@Prop({type: Object as () => IEnvironment, required: true})
+		readonly environment: IEnvironment
 
-	/* Data
-	============================================*/
-	environmentCardSummary?: IEaasiEnvironmentCardSummary;
-	isLoading: boolean = false;
-	hasNoDetails: boolean = false;
+		/* Data
+        ============================================*/
+		environmentCardSummary?: IEaasiEnvironmentCardSummary;
+		isLoading: boolean = false;
+		hasNoDetails: boolean = false;
+		cardSummary: IEaasiResourceSummary = null;
 
-	/* Computed
-	============================================*/
+		/* Computed
+        ============================================*/
 
-	get environmentResourceType() {
-		return resourceTypes.ENVIRONMENT.toUpperCase();
-	}
+		get environmentResourceType() {
+			return resourceTypes.ENVIRONMENT;
+		}
 
-	get envTags(): ITag[] {
-		if (!this.summary) return [];
-		return this.summary.tagGroup;
-	}
+		get envTags(): ITag[] {
+			if (!this.cardSummary) return [];
+			return this.cardSummary.tagGroup;
+		}
 
-	get summary(): IEaasiResourceSummary | null {
-		if (!this.environment) return null;
+		/* Methods
+        ============================================*/
+		async getEnvironmentDetails() {
+			await resourceSvc.getEnvironment(this.environment.envId).then(res => {
+				if (res.error) {
+					this.hasNoDetails = true;
+					this.environmentCardSummary = {
+						hasError: true
+					};
+				} else {
+					this.environmentCardSummary = {
+						title: res.title,
+						description: res.description,
+						archive: res.archive,
+						drives: res.drives,
+						emulator: res.emulator,
+						isInternetEnabled: res.enableInternet,
+						isPrintingEnabled: res.enablePrinting,
+						installedSoftware: res.installedSoftwareIds,
+					};
+				}
+			});
+		}
 
-		resourceSvc.getEnvironment(this.environment.envId).then(res => {
-			if (res.error) {
-				this.hasNoDetails = true;
-				this.environmentCardSummary = {
-					hasError: true
-				};
-			} else {
-				this.environmentCardSummary = {
-					title: res.title,
-					description: res.description,
-					archive: res.archive,
-					drives: res.drives,
-					emulator: res.emulator,
-					isInternetEnabled: res.enableInternet,
-					isPrintingEnabled: res.enablePrinting,
-					installedSoftware: res.installedSoftwareIds,
-				};
-			}
-		});
+		setCardSummary() {
+			if (!this.environment) return null;
 
-		let summary = {
-			id: this.environment.id,
-			title: this.environment.title,
-			tagGroup: [],
-			content: {},
-			subContent: {}
-		} as IEaasiResourceSummary;
+			let summary = {
+				id: this.environment.id,
+				title: this.environment.title,
+				tagGroup: [],
+				content: {},
+				subContent: {}
+			} as IEaasiResourceSummary;
 
-		// TODO: Refactor
-		for (let key in this.environment) {
-			if (key.toLowerCase() === 'archive' || key.toLowerCase() === 'resourcetype') continue;
-
-			let val = this.environment[key];
-
-			if (val && typeof val === 'string' && val !== 'n.a.' && val.length <= 20) {
-
-				summary.content[key] = val;
-				summary.subContent[key] = val;
-
-				if (key.toLowerCase() === 'archive') {
-					if (summary.content[key].toLowerCase() === 'public') {
-						summary.tagGroup.push({
-							icon: 'fa-map-marker-alt',
-							color: 'green',
-							text: 'SAVED'
-						});
+			for (let key in this.environment) {
+				let val = this.environment[key];
+				if (val && typeof val === 'string' && val !== 'n.a.' && val.length <= 20) {
+					summary.subContent[key] = val;
+					if (key.toLowerCase() === 'archive') {
+						if (summary.subContent[key].toLowerCase() === 'public') {
+							summary.tagGroup.push({
+								icon: 'fa-map-marker-alt',
+								color: 'green',
+								text: 'Saved'
+							});
+						}
 					}
 				}
 			}
+
+			for (let key in this.environmentCardSummary) {
+				console.log(key)
+				if (key.toLowerCase() === 'drives') {
+					let driveCountName = '# Drives';
+					summary.content[driveCountName] = this.environmentCardSummary[key].length;
+				} else if (key.toLowerCase() === 'archive') {
+					continue;
+				} else if (key.toLowerCase() === 'isinternetenabled') {
+					let internetEnabled = 'Internet Enabled';
+					summary.content[internetEnabled] = this.environmentCardSummary[key];
+				} else if (key.toLowerCase() === 'isprintingenabled') {
+					let printingEnabled = 'Printing Enabled';
+					summary.content[printingEnabled] = this.environmentCardSummary[key];
+				} else if (key.toLowerCase() === 'description') {
+					summary.content[key] = StringCleaner.stripHTML(this.environmentCardSummary[key]);
+				} else if (key.toLowerCase() === 'installedsoftware') {
+					if (!this.environmentCardSummary[key].length) continue;
+					let installedSoftware = 'Installed Software';
+					summary.content[installedSoftware] = this.environmentCardSummary[key].length;
+				} else {
+					summary.content[key] = this.environmentCardSummary[key];
+				}
+			}
+
+			if (this.hasNoDetails) {
+				summary.tagGroup.push({
+					icon: 'fa-exclamation-triangle',
+					color: 'red',
+					text: 'Error Retrieving Details'
+				});
+			}
+
+			this.cardSummary = summary;
 		}
 
-		if (this.hasNoDetails) {
-			summary.tagGroup.push({
-				icon: 'fa-exclamation-triangle',
-				color: 'red',
-				text: 'Error'
+		setActiveEnvironment(isChecked) {
+			if (isChecked) {
+				this.$store.commit('resource/SET_ACTIVE_ENVIRONMENT', this.environment);
+			} else {
+				this.$store.commit('resource/SET_ACTIVE_ENVIRONMENT', null);
+			}
+			this.$emit('change', isChecked);
+		}
+
+		/* Lifecycle Hooks
+        ============================================*/
+		async created() {
+			this.isLoading = true;
+			this.getEnvironmentDetails().then(async () => {
+				await this.setCardSummary();
+				this.isLoading = false;
 			});
 		}
-		return summary;
-	}
-
-	/* Methods
-	============================================*/
-
-	setActiveEnvironment(isChecked) {
-		if (isChecked) {
-			this.$store.commit('resource/SET_ACTIVE_ENVIRONMENT', this.environment);
-		} else {
-			this.$store.commit('resource/SET_ACTIVE_ENVIRONMENT', null);
-		}
-		this.$emit('change', isChecked);
-	}
-
-	/* Lifecycle Hooks
-	============================================*/
-	created() {
-	}
 
 	/* Watchers
-	============================================*/
+        ============================================*/
 
 }
 
