cab57a5a0032a623cf9d01532ccd938adc23d90d
wesdoyle <wesdoyle08@gmail.com>
2019-10-29 13:43:34 -0500

    Fix run in emulator state bug

--- eaasi-front-end/src/components/resources/ResourceSlideMenu.vue
+++ eaasi-front-end/src/components/resources/ResourceSlideMenu.vue
@@ -50,168 +50,168 @@
 </template>
 
 <script lang="ts">
-import LabeledItemList from '@/components/global/LabeledItem/LabeledItemList.vue';
-import SlideMenu from '@/components/layout/SlideMenu.vue';
-import ResourceSlideMenuService from '@/services/ResourceSlideMenuService';
-import {ILabeledItem} from '@/types/ILabeledItem';
-import {IEaasiResource, IEnvironment} from '@/types/Resource';
-import {IEaasiUser} from 'eaasi-admin';
-import {IAction, IEaasiTab} from 'eaasi-nav';
-import Vue from 'vue';
-import {Component, Prop} from 'vue-property-decorator';
-import {Get, Sync} from 'vuex-pathify';
-import ResourceAction from './ResourceAction.vue';
+	import Vue from 'vue';
+	import LabeledItemList from '@/components/global/LabeledItem/LabeledItemList.vue';
+	import SlideMenu from '@/components/layout/SlideMenu.vue';
+	import ResourceSlideMenuService from '@/services/ResourceSlideMenuService';
+	import {ILabeledItem} from '@/types/ILabeledItem';
+	import {IEaasiResource, IEnvironment} from '@/types/Resource';
+	import {resourceTypes} from '@/utils/constants';
+	import {IEaasiUser} from 'eaasi-admin';
+	import {IAction, IEaasiTab} from 'eaasi-nav';
+	import {Component, Prop} from 'vue-property-decorator';
+	import {Get, Sync} from 'vuex-pathify';
+	import ResourceAction from './ResourceAction.vue';
 
-let menuService = new ResourceSlideMenuService();
+	let menuService = new ResourceSlideMenuService();
 
-@Component({
-	name: 'ResourceSlideMenu',
-	components: {
-		LabeledItemList,
-		ResourceAction,
-		SlideMenu
-	}
-})
-export default class ResourceSlideMenu extends Vue {
+	@Component({
+		name: 'ResourceSlideMenu',
+		components: {
+			LabeledItemList,
+			ResourceAction,
+			SlideMenu
+		}
+	})
+	export default class ResourceSlideMenu extends Vue {
 
-	/* Props
-    ============================================*/
+		/* Props
+        ============================================*/
 
-    @Prop({type: Boolean, required: true})
-    readonly open: boolean
+		@Prop({type: Boolean, required: true})
+		readonly open: boolean
 
-    @Prop({type: Array as () => IEaasiResource[]})
-    readonly resources: IEaasiResource[]
+		@Prop({type: Array as () => IEaasiResource[]})
+		readonly resources: IEaasiResource[]
 
-    /* Computed
-    ============================================*/
+		/* Computed
+        ============================================*/
 
-    @Get('resource/activeEnvironment')
-    readonly environment: IEnvironment
+		@Sync('resource/activeEnvironment')
+		readonly environment: IEnvironment
 
-    @Sync('resource/selectedResources')
-    selectedResources: IEaasiResource[]
+		@Sync('resource/selectedResources')
+		selectedResources: IEaasiResource[]
 
-	@Get('loggedInUser')
-	user: IEaasiUser
+		@Get('loggedInUser')
+		user: IEaasiUser
 
 
-	get onlySelectedResource() : IEaasiResource {
-		if (this.resources.length === 1) {
-			return this.resources[0];
+		get onlySelectedResource() : IEaasiResource {
+			if (this.resources.length === 1) return this.resources[0];
+		}
+
+		/**
+		 * Computes whether or not to show the details tab
+		 */
+		// TODO: Logic for showing details tab
+		get hasDetails() {
+			return true;
+		}
+
+		get areMultipleActiveResourcesSelected() : boolean {
+			return this.resources.length > 1;
+		}
+
+		/**
+		 * Populates the list of Local Actions in the Sidebar
+		 */
+		get localActionsForSelected() {
+			return menuService.getLocalActions(this.selectedResources as IEnvironment[], this.user.roleId);
+		}
+
+		/**
+		 * Populates the list of Node Actions in the Sidebar
+		 */
+		get nodeActionsForSelected() {
+			return menuService.getNodeActions(this.selectedResources as IEnvironment[], this.user.roleId);
+		}
+
+		/* Data
+        ============================================*/
+
+		// TODO: Labeled Items should be derived from the resource
+		labeledItems: ILabeledItem[] = [];
+
+
+		tabs: IEaasiTab[] = [
+			{
+				label: 'Details'
+			},
+			{
+				label: 'Actions'
+			}
+		]
+
+		tab: string = 'Actions'
+
+		/* Methods
+        ============================================*/
+
+		toggleSlide() {
+			this.$emit('toggle');
+		}
+
+		doAction(action: IAction) {
+			if (!action.isEnabled) return;
+
+			switch (action.shortName) {
+				case 'run': {
+					// When Run is clicked, we send to Access Interface @ environmentId
+					if (this.onlySelectedResource.resourceType === resourceTypes.ENVIRONMENT) {
+						let environment = this.onlySelectedResource as IEnvironment;
+						this.$router.push(`/access-interface/${environment.envId}`);
+					}
+					break;
+				}
+				case 'viewDetails': {
+					// When View Details is clicked, we send to Resource Detail view
+					// with the (only) selected resource
+					this.$router.push({
+						name: 'Resource Detail',
+						params: {resource: JSON.stringify(this.onlySelectedResource)}});
+				}
+					break;
+				case 'save': {
+					// When Save is clicked, we show the Save (Replicate) Modal to confirm
+					this.$emit('show-save-modal');
+				}
+					break;
+				default: break;
+			}
 		}
 	}
-
-	/**
-	 * Computes whether or not to show the details tab
-	 */
-	// TODO: Logic for showing details tab
-	get hasDetails() {
-		return true;
-	}
-
-	get areMultipleActiveResourcesSelected() : boolean {
-		return this.resources.length > 1;
-	}
-
-	/**
-	 * Populates the list of Local Actions in the Sidebar
-	 */
-	get localActionsForSelected() {
-		return menuService.getLocalActions(this.selectedResources as IEnvironment[], this.user.roleId);
-	}
-
-	/**
-	 * Populates the list of Node Actions in the Sidebar
-	 */
-	get nodeActionsForSelected() {
-		return menuService.getNodeActions(this.selectedResources as IEnvironment[], this.user.roleId);
-	}
-
-	/* Data
-    ============================================*/
-
-	// TODO: Labeled Items should be derived from the resource
-	labeledItems: ILabeledItem[] = [];
-
-
-    tabs: IEaasiTab[] = [
-    	{
-    		label: 'Details'
-    	},
-    	{
-    		label: 'Actions'
-    	}
-    ]
-
-    tab: string = 'Actions'
-
-    /* Methods
-    ============================================*/
-
-    toggleSlide() {
-    	this.$emit('toggle');
-    }
-
-    doAction(action: IAction) {
-    	if (!action.isEnabled) return;
-
-    	switch (action.shortName) {
-    	case 'run': {
-    		// When Run is clicked, we send to Access Interface @ environmentId
-    		if (this.environment) {
-    			this.$router.push(`/access-interface/${this.environment.envId}`);
-    		}
-    		break;
-    	}
-    	case 'viewDetails': {
-    		// When View Details is clicked, we send to Resource Detail view
-    		// with the (only) selected resource
-    		this.$router.push({
-    			name: 'Resource Detail',
-    			params: {resource: JSON.stringify(this.onlySelectedResource)}});
-    	}
-    	break;
-    	case 'save': {
-    		// When Save is clicked, we show the Save (Replicate) Modal to confirm
-    		this.$emit('show-save-modal');
-    	}
-    		break;
-    	default: break;
-    	}
-    }
-}
 
 </script>
 
 <style lang="scss">
-.resource-slide-menu {
-	background-color: lighten($light-neutral, 60%);
-	position: fixed;
+	.resource-slide-menu {
+		background-color: lighten($light-neutral, 60%);
+		position: fixed;
 
-	.fa-times {
-		cursor: pointer;
+		.fa-times {
+			cursor: pointer;
+		}
 	}
-}
 
-.rsm-header {
-	background-color: #FFFFFF;
-	border-bottom: solid 4px lighten($dark-neutral, 70%);
-}
+	.rsm-header {
+		background-color: #FFFFFF;
+		border-bottom: solid 4px lighten($dark-neutral, 70%);
+	}
 
-.rsm-details {
-	padding: 2.4rem;
-}
+	.rsm-details {
+		padding: 2.4rem;
+	}
 
-.rsm-resource-title {
-	border-top: solid 6px $dark-blue;
-	font-size: 1.7rem;
-	padding: 2rem;
-}
+	.rsm-resource-title {
+		border-top: solid 6px $dark-blue;
+		font-size: 1.7rem;
+		padding: 2rem;
+	}
 
-.rsm-local-actions {
-	border-bottom: solid 4px lighten($light-neutral, 10%);
-	margin-bottom: 3rem;
-}
+	.rsm-local-actions {
+		border-bottom: solid 4px lighten($light-neutral, 10%);
+		margin-bottom: 3rem;
+	}
 
 </style>
