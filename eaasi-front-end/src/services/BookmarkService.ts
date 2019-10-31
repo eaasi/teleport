81dcd402c09a4749b292a33346c12dd678d133f5
Andrii Kost <andrii@Andriis-MacBook-Pro.local>
2019-10-31 15:31:01 -0500

    adds type

--- eaasi-front-end/src/services/BookmarkService.ts
+++ eaasi-front-end/src/services/BookmarkService.ts
@@ -1,23 +1,23 @@
 import BaseHttpService from './BaseHttpService';
-import { BookmarkRequest } from '@/types/Bookmark';
+import { BookmarkRequest, IBookmark } from '@/types/Bookmark';
 import config from '@/config';
 
 class BookmarkService extends BaseHttpService {
 
-    async createBookmark(bookmarkRequest: BookmarkRequest) {
-        const res = await this.post(`${config.REST_API_URL}/bookmark`, bookmarkRequest);
+    async createBookmark(bookmarkRequest: BookmarkRequest): Promise<IBookmark> {
+        const res = await this.post<IBookmark>(`${config.REST_API_URL}/bookmark`, bookmarkRequest);
         if (!res.ok) return null;
         return res.result;
     }
 
-    async removeBookmark(id: number) {
-        const res = await this.delete(`${config.REST_API_URL}/bookmark?id=${id}`);
+    async removeBookmark(id: number): Promise<IBookmark> {
+        const res = await this.delete<IBookmark>(`${config.REST_API_URL}/bookmark?id=${id}`);
         if (!res.ok) return null;
         return res.result;
     }
 
-    async getBookmarks(userID: number) {
-        const res = await this.get(`${config.REST_API_URL}/bookmark?userID=${userID}`);
+    async getBookmarks(userID: number): Promise<IBookmark> {
+        const res = await this.get<IBookmark>(`${config.REST_API_URL}/bookmark?userID=${userID}`);
         if (!res.ok) return null;
         return res.result;
     }
