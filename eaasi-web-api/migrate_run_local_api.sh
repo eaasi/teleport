3959e95b793e104685a5b0c97b6bfed70723b38e
wesdoyle <wesdoyle08@gmail.com>
2019-11-07 11:39:13 -0600

    Temp remove seed

--- eaasi-web-api/migrate_run_local_api.sh
+++ eaasi-web-api/migrate_run_local_api.sh
@@ -10,10 +10,10 @@ cd ./eaasi-web-api
 npx sequelize-cli db:migrate --env=local
 
 # Undo any existing seeds in the database
-npx sequelize-cli db:seed:undo:all --env=local
+# npx sequelize-cli db:seed:undo:all --env=local
 
 # Seed the database
-npx sequelize-cli db:seed:all --env=local
+# npx sequelize-cli db:seed:all --env=local
 
 # Build and run the Express API with nodemon
 npm run local:watch
