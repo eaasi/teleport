70283f1ed488a483345f327f9b3aaefe30e84488
wesdoyle <wesdoyle08@gmail.com>
2019-11-11 13:05:41 -0600

    Create ImportedSoftwareResource object

--- eaasi-web-api/src/data_access/models/index.ts
+++ eaasi-web-api/src/data_access/models/index.ts
@@ -1,10 +1,12 @@
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
 
+import ApplicationLog from '@/data_access/models/app/ApplicationLog';
 import Bookmark from '@/data_access/models/app/Bookmark';
 import EaasiRole from '@/data_access/models/app/EaasiRole';
 import EaasiUser from '@/data_access/models/app/EaasiUser';
 import Emulator from '@/data_access/models/app/Emulator';
+import ImportedSoftwareResource from '@/data_access/models/app/ImportedSoftwareResource';
 import AudioDevice from '@/data_access/models/audio-device/AudioDevice';
 import AudioDeviceHasDriverSoftware from '@/data_access/models/audio-device/AudioDeviceHasDriverSoftware';
 import AudioDeviceHasEquivalent from '@/data_access/models/audio-device/AudioDeviceHasEquivalent';
@@ -174,11 +176,19 @@ export const sequelize = new Sequelize({
 	password: config.password,
 	dialect: config.dialect,
 	models: [
+		// App
+		ApplicationLog,
+		Bookmark,
+		EaasiRole,
+		EaasiUser,
+		Emulator,
+		ImportedSoftwareResource,
+
+		// EaaSI Metadata Models
 		AudioDevice,
 		AudioDeviceHasDriverSoftware,
 		AudioDeviceHasEquivalent,
 		AudioDeviceHasMachineInterface,
-		Bookmark,
 		ChipSet,
 		ColorDepth,
 		ComputingEnvironment,
@@ -215,9 +225,6 @@ export const sequelize = new Sequelize({
 		DisplayDeviceHasDriverSoftware,
 		DisplayInterface,
 		DisplayResolution,
-		EaasiRole,
-		EaasiUser,
-		Emulator,
 		EventType,
 		File,
 		FileExtension,
---
+++ eaasi-web-api/src/types/resource/ImportedSoftwareResource.d.ts
@@ -0,0 +1,6 @@
+export default interface IImportedSoftwareResource {
+	eaasiID: string;
+	environmentTemplateId: string;
+	isUrlSource: boolean;
+	urlSource?: string;
+}
