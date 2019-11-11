aa2386f297db7b1d4fe16f274db6fe51d990a726
Wes Doyle <wesdoyle08@gmail.com>
2019-11-11 16:54:56 +0000

    Fix import

--- eaasi-web-api/src/data_access/models/index.ts
+++ eaasi-web-api/src/data_access/models/index.ts
@@ -1,5 +1,6 @@
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
+
 import Bookmark from '@/data_access/models/app/Bookmark';
 import EaasiRole from '@/data_access/models/app/EaasiRole';
 import EaasiUser from '@/data_access/models/app/EaasiUser';
@@ -167,6 +168,7 @@ import TimezoneLabel from '@/data_access/models/timezone/TimezoneLabel';
 import { Sequelize } from 'sequelize-typescript';
 
 export const sequelize = new Sequelize({
+	host: config.host,
 	database: config.database,
 	username: config.username,
 	password: config.password,
