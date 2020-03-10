f2213bbdc9b075a33cf50fcf77bcd5cbe075d60a
Andrii Kost <andriikost@yahoo.com>
2020-03-10 21:03:55 +0000

    Resolve "OAI-PMH sync bug"

--- eaasi-front-end/src/models/eaas/oaipmh/AddHarvesterRequest.ts
+++ eaasi-front-end/src/models/eaas/oaipmh/AddHarvesterRequest.ts
@@ -1,12 +1,14 @@
-import { IAddHarvesterRequest, IHarvesterStream } from '@/types/Harvesters';
 import config from '@/config';
+import { IAddHarvesterRequest, IHarvesterStream } from '@/types/Harvesters';
 
 export default class AddHarvesterRequest implements IAddHarvesterRequest {
 	name: string;
 	streams: IHarvesterStream[] = [];
 
 	constructor(name: string, endpointUrl: string, syncTypes: string []) {
-		let syncUrl = `${config.EAASI_HOST}/metadata-repositories`;
+		// TODO: Change syncUrl to use EAAS_HOST instead of EAAS_HOST_IP
+		// This breaks because of current networking issue
+		let syncUrl = `${config.EAASI_HOST_IP}/metadata-repositories`;
 		this.name = name;
 		syncTypes.forEach(type => {
 			this.streams.push({
