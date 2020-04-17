39d7dc81519c0a5f19ddb02230e876d78910791b
Andrii Kost <andriikost@yahoo.com>
2020-04-17 13:43:30 -0500

    ecapsulates database actions into a separate shell script

--- eaasi-web-api/start.sh
+++ eaasi-web-api/start.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-# Run database migrations
-npx sequelize-cli db:migrate --env=production
-
-# Undo any existing seeds in the database
-npx sequelize-cli db:seed:undo:all --env=production
-
-# Seed the database
-npx sequelize-cli db:seed:all --env=production
-
 # Build and run the Express API
 npm run prod
