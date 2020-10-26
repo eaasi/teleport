25a1430971c25ef1f2eda71bcd294d6a646aa789
Andrii Kost <andriikost@yahoo.com>
2020-10-26 12:53:49 -0500

    pipeline-errro uncommenting wrong bad test

--- eaasi-front-end/tests/unit/components/admin/AdminMenu.spec.ts
+++ eaasi-front-end/tests/unit/components/admin/AdminMenu.spec.ts
@@ -1,68 +1,64 @@
-// import AdminMenu from '@/components/admin/AdminMenu.vue';
-// import AdminMenuItem from '@/components/admin/AdminMenuItem.vue';
-// import PermissionResolver from '@/services/Permissions/PermissionResolver';
-// import localAdminStore from '@/store/admin-store';
-// import globalStore from '@/store/global-store';
-// import { userRoles } from '@/utils/constants';
-// import { createLocalVue, shallowMount } from '@vue/test-utils';
-// import Vuex from 'vuex';
-// import pathify from 'vuex-pathify';
+import AdminMenu from '@/components/admin/AdminMenu.vue';
+import AdminMenuItem from '@/components/admin/AdminMenuItem.vue';
+import PermissionResolver from '@/services/Permissions/PermissionResolver';
+import localAdminStore from '@/store/admin-store';
+import globalStore from '@/store/global-store';
+import { userRoles } from '@/utils/constants';
+import { createLocalVue, shallowMount } from '@vue/test-utils';
+import Vuex from 'vuex';
+import pathify from 'vuex-pathify';
 
-// const localVue = createLocalVue();
-// localVue.use(Vuex);
+const localVue = createLocalVue();
+localVue.use(Vuex);
 
 describe('AdminMenu.vue', () => {
+	let store;
+	let mutations;
+	let getters;
 
-	it('TODO: REMOVE', () => {
-		expect(true).toBe(true);
+	beforeEach(() => {
+		// Note: this is testing as if a user is admin (role 1)
+		let permissionResolver = new PermissionResolver(userRoles.ADMIN);
+
+		getters = {
+			permissions: () => permissionResolver,
+		};
+
+		mutations = {
+			SET_ACTIVE_USER: jest.fn()
+		};
+
+		store = new Vuex.Store({
+			modules: {
+				// @ts-ignore
+				admin: localAdminStore,
+				// @ts-ignore
+				global: globalStore
+			},
+			state: {
+				appVersion: '',
+				loggedInUser: {'id': 123}
+			},
+			plugins: [pathify.plugin],
+			getters
+		});
 	});
-	// let store;
-	// let mutations;
-	// let getters;
 
-	// beforeEach(() => {
-	// 	// Note: this is testing as if a user is admin (role 1)
-	// 	let permissionResolver = new PermissionResolver(userRoles.ADMIN);
+	it('Displays all menu items', () => {
+		const wrapper = shallowMount(AdminMenu, { store, localVue });
+		const userMenuItems = wrapper.vm.$data['userMenuItems'];
+		const menuItems = wrapper.vm.$data['menuItems'];
+		const allItems = userMenuItems.concat(menuItems);
+		expect(wrapper.findAll(AdminMenuItem).length).toBe(allItems.length);
+	});
 
-	// 	getters = {
-	// 		permissions: () => permissionResolver,
-	// 	};
+	it('Contains Node Management section', () => {
+		const wrapper = shallowMount(AdminMenu, { store, localVue });
+		expect(wrapper.contains('Node Management'));
+	});
 
-	// 	mutations = {
-	// 		SET_ACTIVE_USER: jest.fn()
-	// 	};
-
-	// 	store = new Vuex.Store({
-	// 		modules: {
-	// 			// @ts-ignore
-	// 			admin: localAdminStore,
-	// 			// @ts-ignore
-	// 			global: globalStore
-	// 		},
-	// 		state: {
-	// 			appVersion: '',
-	// 			loggedInUser: {'id': 123}
-	// 		},
-	// 		plugins: [pathify.plugin],
-	// 		getters
-	// 	});
-	// });
-
-	// it('Displays all menu items', () => {
-	// 	const wrapper = shallowMount(AdminMenu, { store, localVue });
-	// 	const userMenuItems = wrapper.vm.$data['userMenuItems'];
-	// 	const menuItems = wrapper.vm.$data['menuItems'];
-	// 	const allItems = userMenuItems.concat(menuItems);
-	// 	expect(wrapper.findAll(AdminMenuItem).length).toBe(allItems.length);
-	// });
-
-	// it('Contains Node Management section', () => {
-	// 	const wrapper = shallowMount(AdminMenu, { store, localVue });
-	// 	expect(wrapper.contains('Node Management'));
-	// });
-
-	// it('Contains Node User Administration section', () => {
-	// 	const wrapper = shallowMount(AdminMenu, { store, localVue });
-	// 	expect(wrapper.contains('Node User Administration'));
-	// });
+	it('Contains Node User Administration section', () => {
+		const wrapper = shallowMount(AdminMenu, { store, localVue });
+		expect(wrapper.contains('Node User Administration'));
+	});
 });
