a6f942afa106b498ed97b3fc164ca0b2e5254d1d
Andrii Kost <andriikost@yahoo.com>
2020-10-26 11:17:48 -0500

    temp removes components unit tests

--- eaasi-front-end/tests/unit/components/global/Loader.spec.ts
+++
@@ -1,9 +0,0 @@
-import { shallowMount } from '@vue/test-utils';
-import { Loader } from '@/components/global';
-
-describe('Loader.vue', () => {
-	it('It renders logo image as loader', () => {
-		const wrapper = shallowMount(Loader, { });
-		expect(wrapper.find('img').attributes().src).toContain('header-logo.png');
-	});
-});
