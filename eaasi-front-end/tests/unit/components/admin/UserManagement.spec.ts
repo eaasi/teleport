6138a046e937d9e5f7c948c42dca3d014f2ff79a
Andrii Kost <andriikost@yahoo.com>
2020-10-26 13:08:31 -0500

    adds fake admin store

--- eaasi-front-end/tests/unit/components/admin/UserManagement.spec.ts
+++ eaasi-front-end/tests/unit/components/admin/UserManagement.spec.ts
@@ -1,64 +1,60 @@
-// import UserList from '@/components/admin/users/UserList.vue';
-// import UserManagement from '@/components/admin/users/UserManagement.vue';
-// import PermissionResolver from '@/services/Permissions/PermissionResolver';
-// import adminStore from '@/store/admin-store';
-// import globalStore from '@/store/global-store';
-// import { userRoles } from '@/utils/constants';
-// import { createLocalVue, shallowMount } from '@vue/test-utils';
-// import Vuex from 'vuex';
-// import pathify from 'vuex-pathify';
-// import { makeAdminStoreState } from '../../store-helpers';
+import UserList from '@/components/admin/users/UserList.vue';
+import UserManagement from '@/components/admin/users/UserManagement.vue';
+import PermissionResolver from '@/services/Permissions/PermissionResolver';
+import globalStore from '@/store/global-store';
+import { userRoles } from '@/utils/constants';
+import { createLocalVue, shallowMount } from '@vue/test-utils';
+import Vuex from 'vuex';
+import pathify from 'vuex-pathify';
+import { makeAdminStoreState } from '../../store-helpers';
+import fakeAdminStore from '../../store/fake-admin-store';
 
-// const localVue = createLocalVue();
+const localVue = createLocalVue();
 
-// localVue.use(Vuex);
+localVue.use(Vuex);
 
-// const $colors = {
-// 	'lightNeutral': '#bbb'
-// };
+const $colors = {
+	'lightNeutral': '#bbb'
+};
 
 describe('UserManagement.vue', () => {
+	let store;
+	let getters;
 
-	it('TODO: REMOVE', () => {
-		expect(true).toBe(true);
+	beforeEach(() => {
+		// Note: this is testing as if a user is admin (role 1)
+		let permissionResolver = new PermissionResolver(userRoles.ADMIN);
+
+		let localAdminStore = fakeAdminStore;
+
+		getters = {
+			permissions: () => permissionResolver,
+		};
+
+		localAdminStore.state = makeAdminStoreState(10);
+
+		store = new Vuex.Store({
+			modules: {
+				// @ts-ignore
+				admin: localAdminStore,
+				// @ts-ignore
+				global: globalStore
+			},
+			plugins: [pathify.plugin],
+			getters
+		});
 	});
-// 	let store;
-// 	let getters;
 
-// 	beforeEach(() => {
-// 		// Note: this is testing as if a user is admin (role 1)
-// 		let permissionResolver = new PermissionResolver(userRoles.ADMIN);
+	it('Renders a List of Users', () => {
+		const wrapper = shallowMount(UserManagement, {
+			mocks: {$colors},
+			localVue,
+			propsData: {
+				showCreateModal: false,
+			},
+			store
+		});
 
-// 		let localAdminStore = adminStore;
-
-// 		getters = {
-// 			permissions: () => permissionResolver,
-// 		};
-
-// 		localAdminStore.state = makeAdminStoreState(10);
-
-// 		store = new Vuex.Store({
-// 			modules: {
-// 				// @ts-ignore
-// 				admin: localAdminStore,
-// 				// @ts-ignore
-// 				global: globalStore
-// 			},
-// 			plugins: [pathify.plugin],
-// 			getters
-// 		});
-// 	});
-
-// 	it('Renders a List of Users', () => {
-// 		const wrapper = shallowMount(UserManagement, {
-// 			mocks: {$colors},
-// 			localVue,
-// 			propsData: {
-// 				showCreateModal: false,
-// 			},
-// 			store
-// 		});
-
-// 		expect(wrapper.find(UserList).exists()).toBe(true);
-// 	});
+		expect(wrapper.find(UserList).exists()).toBe(true);
+	});
 });
