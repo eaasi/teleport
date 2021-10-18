8ad629eab2b8aadf0d4de70e4464d70040dc9afd
Oleg Stobbe <oleg@openslx.com>
2021-10-18 21:17:04 +0200

    Rework containerized service 'eaasi-web-api'

--- eaasi-web-api/scripts/start.sh
+++ eaasi-web-api/scripts/start.sh
@@ -1,10 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
-# Run database migrations
+set -eu
+
+echo "Waiting for database at '${DB_HOST}:${DB_PORT}'..."
+timeout 60s sh -c 'while ! nc -z $0 $1; do sleep 1; done' "${DB_HOST}" "${DB_PORT}"
+
+echo "Running database migrations..."
 npx sequelize-cli db:migrate --env=production
 
-# Seed the database
-npx sequelize-cli db:seed:all --env=production
-
-# Start the eaasi_web_api application in production mode
-npm run prod
+echo "Executing 'node $@'..."
+exec node "$@"
