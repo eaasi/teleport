babb919e5856a5241606aa826b8cba746198532c
Oleg Stobbe <oleg@openslx.com>
2021-10-28 03:25:14 +0200

    Remove deprecated helper for user-token validation

--- eaasi-front-end/src/utils/auth.ts
+++ eaasi-front-end/src/utils/auth.ts
@@ -1,22 +1,5 @@
-import jwt from 'jsonwebtoken';
-import { IEaasiUser } from 'eaasi-admin';
 import Cookies from 'js-cookie';
 import config from '@/config';
-
-const SECRET = process.env.VUE_APP_JWT_SECRET;
-const JWT_NAME = process.env.VUE_APP_JWT_NAME;
-
-export function validateUserToken(token: string): IEaasiUser {
-	try {
-		let user = jwt.verify(token, SECRET) as IEaasiUser;
-		localStorage.setItem(JWT_NAME, token);
-		return user;
-	} catch(err) {
-		console.warn('JWT Error', err);
-		localStorage.removeItem(JWT_NAME);
-		return null;
-	}
-}
 
 export function getUserToken(): string {
 	return Cookies.get(config.JWT_NAME);
