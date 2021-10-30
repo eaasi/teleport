6cf48b3f672702ba3c21eb6cab3f2501d151d710
Oleg Stobbe <oleg@openslx.com>
2021-10-30 20:59:42 +0200

    Fix eaasi-front-end's failing unit-tests

--- eaasi-front-end/tests/unit/components/layout/SlideMenu.spec.ts
+++ eaasi-front-end/tests/unit/components/layout/SlideMenu.spec.ts
@@ -18,18 +18,18 @@ describe('SlideMenu.vue', () => {
 		expect(menu.attributes('style')).toBe('transition: transform 1s ease-out;');
 	});
 
-	it('updates its style attribute when speed is changed', () => {
-		menu.setProps({speed: 2});
+	it('updates its style attribute when speed is changed', async () => {
+		await menu.setProps({speed: 2});
 		expect(menu.attributes('style')).toBe('transition: transform 2s ease-out;');
 	});
 
-	it('has an open class when opened', () => {
-		menu.setProps({open: true});
+	it('has an open class when opened', async () => {
+		await menu.setProps({open: true});
 		expect(menu.classes().some(x => x === 'open')).toBe(true);
 	});
 
-	it('has no open class when closed', () => {
-		menu.setProps({open: false});
+	it('has no open class when closed', async () => {
+		await menu.setProps({open: false});
 		expect(menu.classes().some(x => x === 'open')).toBe(false);
 	});
 
