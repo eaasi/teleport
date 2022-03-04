7a9568109987c6dcf31eb33ee6e368397012e90b
Ekaterina Sosinovich <ekaterina.sosinovich@duallab.com>
2022-03-04 16:10:58 +0300

    Fix issue with empty software data

--- eaasi-front-end/src/components/emulation-project/EmulationProjectModeScreen.vue
+++ eaasi-front-end/src/components/emulation-project/EmulationProjectModeScreen.vue
@@ -81,6 +81,7 @@ export default class EmulationProjectModeScreen extends Vue {
 		this.environment = null;
 		this.selected = [];
 		this.$emit('reset');
+		this.selectingResourceTypes = [];
 	}
 
 }
