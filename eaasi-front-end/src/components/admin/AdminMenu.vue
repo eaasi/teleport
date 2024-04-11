7b472dcad53a5ea63062cf58725c2baa69dc7ae9
Oleg Stobbe <oleg@openslx.com>
2024-04-11 23:26:43 +0200

    Rework layout of build and user information, improve styling of admin-menu

--- eaasi-front-end/src/components/admin/AdminMenu.vue
+++ eaasi-front-end/src/components/admin/AdminMenu.vue
@@ -1,7 +1,6 @@
 <template>
 	<div class="admin-menu" v-if="isViewable">
-		<div class="menu-divider"></div>
-		<h2 class="admin-menu-heading">Node Management</h2>
+		<h2 class="admin-menu-heading" style="margin-top: 1rem;">Node Management</h2>
 		<admin-menu-item v-for="i in menuItems" :key="i.route" :item="i" />
 		<div class="menu-divider" v-if="isUserMenuViewable"></div>
 		<h2 class="admin-menu-heading" v-if="isUserMenuViewable">Node User Administration</h2>
@@ -9,11 +8,14 @@
 		<div class="menu-divider"></div>
 		<h2 class="admin-menu-heading">Application Version</h2>
 		<div class="app-version">{{ appVersion }}</div>
-		<div class="addition-app-info" v-if="!isProduction">
+		<h2 class="admin-menu-heading">Front-End Build</h2>
+		<div class="app-version">{{ frontEndBuild }}</div>
+		<h2 class="admin-menu-heading">Back-End Build</h2>
+		<div class="app-version">{{ backEndBuild }}</div>
+		<h2 class="admin-menu-heading">User Info</h2>
+		<div class="user-info">
 			<div>User: {{ user.username }}</div>
 			<div>Role: {{ roleType }}</div>
-			<div>Build: {{ BEBuild }}</div>
-			<div>UI-Build: {{ commitHash }} </div>
 		</div>
 	</div>
 </template>
@@ -47,12 +49,8 @@ export default class AdminMenu extends Vue {
 
 	/* Data
 	============================================*/
-	commitHash: string = process.env.VUE_APP_GIT_HASH;
-	BEBuild: string = '';
-
-	get isProduction(): boolean {
-		return process.env.NODE_ENV === 'production';
-	}
+	frontEndBuild: string = process.env.VUE_APP_BUILD_VERSION;
+	backEndBuild: string = '';
 
 	get isViewable(): boolean {
 		if (!this.user) return false;
@@ -138,7 +136,7 @@ export default class AdminMenu extends Vue {
 	readonly appVersion: string;
 
 	async getBEBuildVersion() {
-		this.BEBuild = await this.$store.dispatch('admin/getBEBuildVersion');
+		this.backEndBuild = await this.$store.dispatch('admin/getBEBuildVersion');
 	}
 
 	/* Methods
@@ -167,23 +165,21 @@ export default class AdminMenu extends Vue {
 
 	h2 {
 		color: $teal;
-		font-size: 1.3rem;
-		margin-top: -10px;
+		font-size: 1.4rem;
+		margin-top: 3rem;
 		text-transform: uppercase;
 	}
 }
 
 .menu-divider {
 	background-color: darken($teal, 55%);
-	height: 2px;
+	height: 1px;
 	margin: 2rem 0;
 }
 
-.addition-app-info {
-	margin: 20px 0;
-
+.user-info {
 	div {
-		margin: 10px 0;
+		margin: 1rem 0;
 	}
 }
 
