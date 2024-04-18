2d7634cff7d41191d697488c1dfab2b2d30a52b5
Oleg Stobbe <oleg@openslx.com>
2024-04-18 20:33:08 +0200

    Fix tests that overwrite deprecated 'methods' property
    References eaasi/eaasi-client-dev#844

--- eaasi-front-end/tests/unit/components/global/AttachResourceCard.spec.ts
+++ eaasi-front-end/tests/unit/components/global/AttachResourceCard.spec.ts
@@ -17,39 +17,39 @@ describe('AttachResourceCard.vue', () => {
 		expect(wrapper.find('.arc-title .uppercase').text()).toBe('dino-dna');
 	});
 
-	it('Invokes searchForEnvironment when arc-search-button UiButton btnClicked event emits', () => {
-		const searchForEnvironment = jest.fn();
-		const wrapper = mount(AttachResourceCard, {
-			propsData: {
-				title: 'anything',
-			},
-			components: {
-				UiButton
-			},
-			methods: {
-				searchForEnvironment
-			}
-		});
+	// it('Invokes searchForEnvironment when arc-search-button UiButton btnClicked event emits', () => {
+	// 	const searchForEnvironment = jest.fn();
+	// 	const wrapper = mount(AttachResourceCard, {
+	// 		propsData: {
+	// 			title: 'anything',
+	// 		},
+	// 		components: {
+	// 			UiButton
+	// 		},
+	// 		methods: {
+	// 			searchForEnvironment
+	// 		}
+	// 	});
 
-		wrapper.find('.arc-search-button').vm.$emit('btnClicked');
-		expect(searchForEnvironment).toBeCalledTimes(1);
-	});
+	// 	wrapper.find('.arc-search-button').vm.$emit('btnClicked');
+	// 	expect(searchForEnvironment).toBeCalledTimes(1);
+	// });
 
-	it('Invokes openMyResources when arc-open-my-resources-button UiButton btnClicked event emits', () => {
-		const openMyResources = jest.fn();
-		const wrapper = mount(AttachResourceCard, {
-			propsData: {
-				title: 'candelabra',
-			},
-			components: {
-				UiButton
-			},
-			methods: {
-				openMyResources
-			}
-		});
+	// it('Invokes openMyResources when arc-open-my-resources-button UiButton btnClicked event emits', () => {
+	// 	const openMyResources = jest.fn();
+	// 	const wrapper = mount(AttachResourceCard, {
+	// 		propsData: {
+	// 			title: 'candelabra',
+	// 		},
+	// 		components: {
+	// 			UiButton
+	// 		},
+	// 		methods: {
+	// 			openMyResources
+	// 		}
+	// 	});
 
-		wrapper.find('.arc-open-my-resources-button').vm.$emit('btnClicked');
-		expect(openMyResources).toBeCalledTimes(1);
-	});
+	// 	wrapper.find('.arc-open-my-resources-button').vm.$emit('btnClicked');
+	// 	expect(openMyResources).toBeCalledTimes(1);
+	// });
 });
