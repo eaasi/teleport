f19bf3ab3d4a9d0716efb010ff7a09832279c885
Oleg Stobbe <oleg@openslx.com>
2024-04-19 22:16:58 +0200

    Fix custom validator for type AlertType
    References eaasi/eaasi-client-dev#844

--- eaasi-front-end/src/types/validators/AlertType.validator.ts
+++ eaasi-front-end/src/types/validators/AlertType.validator.ts
@@ -1,3 +1,3 @@
 export default function isValidAlert(value): boolean {
-	return ['success', 'info', 'warning', 'error'].indexOf(value) !== -1;
+	return ['success', 'info', 'warning', 'error', 'neutral'].indexOf(value) !== -1;
 }
