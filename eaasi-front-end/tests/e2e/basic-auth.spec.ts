d9d01c7df99c86018075558e43b1ab9b7684ece0
Darya Romanchuk <darya.romanchuk@duallab.com>
2024-08-22 18:19:38 +0300

    Add basic authentication tests
    References: eaasi/eaasi-client-dev#887

---
+++ eaasi-front-end/tests/e2e/basic-auth.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect } from '@playwright/test';
+
+test('Basic auth', async ({ page, browser }) => {
+	await test.step('should login', async () => {
+		await page.goto('https://dev.eaasi.duallab.com:8543');
+		await page.locator('#username').fill(process.env.VUE_APP_TEST_KEYCLOAK_USERNAME);
+		await page.locator('#password').fill(process.env.VUE_APP_TEST_KEYCLOAK_PASSWORD);
+		await page.locator('#kc-login').click();
+		await page.waitForURL('https://dev.eaasi.duallab.com:8543/dashboard');
+		await expect(page.locator('#dh-eaasi-welcome')).toBeVisible();
+	});
+
+	await test.step('go to my resources', async () => {
+		await page.goto('https://dev.eaasi.duallab.com:8543/resources/my-resources');
+		const locatorResourcesPage = page.locator('.page-title');
+		await expect(locatorResourcesPage).toHaveText('My Resources');
+	});
+
+	await test.step('should logout', async () => {
+		await page.locator('.hmd-user').click();
+		await page.getByText('Log Out').click();
+		const orderSent = page.locator('#kc-form');
+		await orderSent.waitFor();
+		const locator = page.locator('h2');
+		await expect(locator).toHaveText('Emulation-as-a-Service-Infrastructure');
+	});
+});
