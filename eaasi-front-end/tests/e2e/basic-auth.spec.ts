d4075a801a340181a96a00231efee88185623d4a
Oleg Stobbe <oleg@openslx.com>
2024-09-07 20:51:19 +0200

    Make base URL for eaasi-front-end's E2E tests configurable

--- eaasi-front-end/tests/e2e/basic-auth.spec.ts
+++ eaasi-front-end/tests/e2e/basic-auth.spec.ts
@@ -2,16 +2,16 @@ import { test, expect } from '@playwright/test';
 
 test('Basic auth', async ({ page, browser }) => {
 	await test.step('should login', async () => {
-		await page.goto('https://dev.eaasi.duallab.com:8543');
+		await page.goto('/');
 		await page.locator('#username').fill(process.env.VUE_APP_TEST_KEYCLOAK_USERNAME);
 		await page.locator('#password').fill(process.env.VUE_APP_TEST_KEYCLOAK_PASSWORD);
 		await page.locator('#kc-login').click();
-		await page.waitForURL('https://dev.eaasi.duallab.com:8543/dashboard');
+		await page.waitForURL('/dashboard*');
 		await expect(page.locator('#dh-eaasi-welcome')).toBeVisible();
 	});
 
 	await test.step('go to my resources', async () => {
-		await page.goto('https://dev.eaasi.duallab.com:8543/resources/my-resources');
+		await page.goto('/resources/my-resources');
 		const locatorResourcesPage = page.locator('.page-title');
 		await expect(locatorResourcesPage).toHaveText('My Resources');
 	});
