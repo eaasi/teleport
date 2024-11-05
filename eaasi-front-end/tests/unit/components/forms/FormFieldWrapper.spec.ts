86a240d18ba7e84ed653758dc4c744b1d2b4fff7
Darya Romanchuk <darya.romanchuk@duallab.com>
2024-11-05 16:11:53 +0300

    Fixed tests for form field wrapper

--- eaasi-front-end/tests/unit/components/forms/FormFieldWrapper.spec.ts
+++ eaasi-front-end/tests/unit/components/forms/FormFieldWrapper.spec.ts
@@ -8,8 +8,6 @@ describe('FormFieldWrapper.vue', () => {
 	it('Displays an error message when error passed as prop', () => {
 		const wrapper = shallowMount(FormFieldWrapper, {
 			propsData: {
-				hideLabel: true,
-				label: '',
 				error: 'uh oh',
 				required: false
 			},
@@ -23,59 +21,12 @@ describe('FormFieldWrapper.vue', () => {
 	it('Displays no error message when no error passed as prop', () => {
 		const wrapper = shallowMount(FormFieldWrapper, {
 			propsData: {
-				hideLabel: true,
-				label: '',
 				error: undefined,
 				required: false
 			},
 		});
 
 		expect(wrapper.find('.error')
-			.exists())
-			.toBe(false);
-	});
-
-	it('Displays a label when label passed as prop and not hidden', () => {
-		const wrapper = shallowMount(FormFieldWrapper, {
-			propsData: {
-				hideLabel: false,
-				label: 'good stuff',
-				error: undefined,
-				required: false
-			},
-		});
-
-		expect(wrapper.find('.eaasi-label')
-			.text())
-			.toContain('good stuff');
-	});
-
-	it('Hides a label when label passed as prop and hidden is true', () => {
-		const wrapper = shallowMount(FormFieldWrapper, {
-			propsData: {
-				hideLabel: true,
-				label: 'good stuff',
-				error: undefined,
-				required: false
-			},
-		});
-
-		expect(wrapper.find('.eaasi-label')
-			.exists())
-			.toBe(false);
-	});
-
-	it('Hides a label when no label passed as prop even if hidden is false', () => {
-		const wrapper = shallowMount(FormFieldWrapper, {
-			propsData: {
-				hideLabel: false,
-				label: undefined,
-				error: undefined,
-				required: false
-			},
-		});
-
-		expect(wrapper.find('.eaasi-label')
 			.exists())
 			.toBe(false);
 	});
